11af94ca55e639acb2db95a2ea494871d57d24fd
kafitramarna <46441882+kafitramarna@users.noreply.github.com>
2019-03-09 17:31:52 +0700

    Update nmap.sh

--- nmap.sh
+++ nmap.sh
@@ -1,108 +1,59 @@
 #!/bin/bash
-echo "            _,.-------.,_ "
+#///////////////////////////////////////////////////////////////////////////////
+#///////////////////////////////////////////////////////////////////////////////
+#////            _______   _     _   _______  ____      _______             ////
+#////           |  _____| | |   | | |  _____||__  |    |_____  |            ////
+#////           | |       | |   | | | |         | |          | |            ////
+#////           | |_____  | |___| | | |_____    | |     _____| |            ////
+#////           |_____  | |_____  | |_____  |   | |    |  _____|            ////
+#////                 | |       | |       | |   | |    | |                  ////
+#////            _____| |       | |  _____| | __| |__  | |_____             ////
+#////           |_______|       |_| |_______||_______| |_______|            ////
+#////                                                                       ////
+#///////////////////////////////////////////////////////////////////////////////
+#///////////////////////////////////////////////////////////////////////////////
+echo "           ___.-------.___"
 sleep 0.03
-echo "        ,;~'             '~;, "
+echo "       _.-' ___.--;--.___ '-._ "
 sleep 0.03
-echo "      ,;                     ;, "
+echo "    .-' _.-'  /  .+.  \  '-._ '-. "
 sleep 0.03
-echo "     ;                         ; "
+echo "  .' .-'      |-|-o-|-|      '-. '. "
 sleep 0.03
-echo "    ,'                         ', "
+echo " (_ <O__      \  '+'  /      __O> _) "
 sleep 0.03
-echo "   ,;                           ;, "
+echo "   '--._''-..__'._|_.'__..-''_.--' "
 sleep 0.03
-echo "   ; ;      .           .      ; ; "
+echo "         ''--._________.--'' "
 sleep 0.03
-echo "   | ;   ______       ______   ; | "
-sleep 0.03
-echo "   |  '/~'     ~' . '~     '~\'  | "
-sleep 0.03
-echo "   |  ~  ,-~~~^~, | ,~^~~~-,  ~  | "
-sleep 0.03
-echo "    |   |        }:{        |   | "
-sleep 0.03
-echo "    |   l       / | \       !   | "
-sleep 0.03
-echo "    .~  (__,.--' .^. '--.,__)  ~. "
-sleep 0.03
-echo "    |     ---;' / | \ ';---     | "
-sleep 0.03
-echo "     \__.       \/^\/       .__/ "
-sleep 0.03
-echo "      V| \                 / |V "
-sleep 0.03
-echo "       | |T~\___!___!___/~T| | "
-sleep 0.03
-echo "       | |'IIII_I_I_I_IIII'| | "
-sleep 0.03
-echo "       |  \,III I I I III,/  | "
-sleep 0.03
-echo "        \   '~~~~~~~~~~'    / "
-sleep 0.03
-echo "          \   .       .   / "
-sleep 0.03
-echo "            \.    ^    ./ " 
-sleep 0.03
-echo "              ^~~~^~~~^  "
-sleep 0.03
-echo "  ================================"
+echo " ==================================="
 sleep 2
-echo "  ==       Skull Project        =="
-sleep 0.7
-echo "  ================================"
-echo " Skull Project adalah Tools phising "
-echo " Instagram via best9 "
-get_url=$(curl -s http://zlucifer.com/api/best9.php)
-cek='curl -s '$get_url # check status
-response=`curl -s -o /dev/null -w "%{http_code}" $cek`
-#echo $response
-if [[ $response != *pre* ]]; then
-    echo
-    echo "Website Offline/Restart untuk sementara"
+echo " ==           Nmap Lite           =="
+sleep 0.07
+echo " ==================================="
+echo
+echo "Masukan IP target"
+read target
+echo
+echo "Apakah target "$target" sudah benar?"
+echo "y/n?"
+read confirm
+echo $confirm
+if [ $confirm = "y" ]; then
+echo "Melakukan scanning"
+i=0
+max=100
+while [ $i -le $max ]; do
+echo -ne "\nScanning $target percentage : $i% "
+sleep 0.03
+if [ $i -eq 100 ]; then
+echo -ne " [complete!]\n"
+echo "========================================"
+curl -s http://zlucifer.hol.es/Project_nmap_lite/index.php?target=$target
+echo "======================================="
+fi
+let i++
+done
 else
-    echo
-    echo "Cara penggunaan :"
-    echo "1. Copy link ini : www.best9-insta.pe.hu"
-    echo "2. Kirim link yang sudah di copy ke target"
-    echo "3. Jika korban sudah terkena bisa langsung di cek"
-    echo 
-    echo "Ingin melakukan pengecekan?"
-    echo "y/n?"
-    read pengecekan
-    echo
-    if [ $pengecekan = "y" ]; then
-            echo "Silahkan masukan username target"
-    echo contoh hack.id_
-    read target # masukin user target
-    echo
-    echo Apakah username $target "sudah benar?"
-    echo y/n?
-    read confirm
-    echo
-    if [ $confirm = "y" ]; then
-        echo Melakukan pencarian password username : $target
-        i=0
-        max=100
-        while [ $i -le $max ]; do
-        echo -ne "\nScanning percentage : $i% "
-        sleep 0.03
-        if [ $i -eq 100 ]; then
-            echo -ne " [complete!]\n"
-            echo "Jangan close aplikasi sebelum scan selesai"            
-            echo "========================================"
-            cek_target=`curl -s $get_url/instagram.php?cari=$target`
-            echo -e $cek_target
-            echo "======================================="
-            echo " Gunakan tools dengan bijak"
-            echo " -zLucifer"
-            echo "======================================="
-            fi
-            let i++
-            done
-        else
-            echo "Kesalahan"
-        fi
-    else
-        echo "Terimakasih sudah menggunakan Skull Project"
-    fi    
+echo "Kesalahan"
 fi
